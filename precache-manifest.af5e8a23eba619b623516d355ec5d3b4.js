138853f3ccc4a7e95f6a8fe9bb97c146db5091c4
Alexander <1alexvash@gmail.com>
2019-06-29 19:03:08 +0300

    Updates

---
+++ precache-manifest.af5e8a23eba619b623516d355ec5d3b4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca5bb5196e12a40b2514eb08ddd26447",
+    "url": "/Currencies/index.html"
+  },
+  {
+    "revision": "4c66fc99008db44ca863",
+    "url": "/Currencies/static/css/main.a60d1e8e.chunk.css"
+  },
+  {
+    "revision": "5f13eae82bdb6457fc8e",
+    "url": "/Currencies/static/js/2.9bd2e2ca.chunk.js"
+  },
+  {
+    "revision": "4c66fc99008db44ca863",
+    "url": "/Currencies/static/js/main.5c27e2b5.chunk.js"
+  },
+  {
+    "revision": "be81485c3ca729942ea2",
+    "url": "/Currencies/static/js/runtime~main.8cccf251.js"
+  }
+]);
